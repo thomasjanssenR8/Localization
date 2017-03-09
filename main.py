af6d7a6105b1920eb488f98a899e46988f0bc0b6
thomasjanssenR8 <r8.thomas@hotmail.com>
2017-03-09 11:53:09 +0100

    Multiple coordinates (resultsPerPage)  from http request are being processed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 
+# @TODO: Tel aantal request per 24 uur: 3
+
 
 class Localization:
     userID = ""
@@ -42,32 +44,40 @@ class Localization:
         print(r.text)  # print string
         return r
 
-    def decodeObject(self, r):  #@TODO: meerdere coordinaten opslaan in array of list
+    def decodeObject(self, r):
         data = r.json()
+
         resultCount = data['resultCount']
-        results     = data['results']
-        trilat      = results[0]['trilat']
-        trilong     = results[0]['trilong']
-        position    = [trilat, trilong]
         print("ResultCount = " + str(resultCount))
-        print("Latitude = " + str(trilat) + ", Longitude = " + str(trilong))
+
+        results = data['results']
+
+        count = 0
+        positions = []
+
+        for result in range(0, len(results)):
+            trilat = results[count]['trilat']
+            trilong = results[count]['trilong']
+            print("Latitude = " + str(trilat) + ", Longitude = " + str(trilong))
+            position = [trilat, trilong]
+            positions.append(position)
+            count += 1
 
         # param1 = data["success"]  # Use this in case daily limit of requests is reached
         # param2 = data["error"]
         # print(param1)
         # print(param2)
-        # position = [param1, param2]
+        # positions = [param1, param2]
 
-        return position
+        return positions
 
 
-
+# Authenticate with username + password, perform HTTP-request tot Wigle-server, and find coordinates in JSON-string
+# -----------------------------------------------------------------------------------------------------------------
 l = Localization("user", "pw")
 
-l.setResultsPerPage(input("Give amount of results per page: "))
-
+l.resultsPerPage = input("Give amount of results per page: ")
 l.ssid = input("Give the SSID you are looking for: ")
-
 url = l.createURL(l.ssid, l.resultsPerPage)
 print("Created URL:   " + url)
 
@@ -77,8 +87,7 @@ position = l.decodeObject(request)  # response unmarshalling (this function also
 
 
 # Use this in case daily limit of requests is reached:
-# data = '{"resultCount":1,"last":1,"success":true,"results":[{"trilat":51.22378540,"trilong":4.40892172,"ssid":"UAntwerpen","qos":7,"transid":"20120419-00187","firsttime":"2012-04-19T23:05:18.000Z","lasttime":"2015-02-22T11:26:18.000Z","lastupdt":"2015-02-22T09:28:02.000Z","netid":"00:0B:86:26:79:C5","name":null,"type":"infra","comment":null,"wep":"2","channel":6,"bcninterval":0,"freenet":"?","dhcp":"?","paynet":"?","userfound":false}],"first":1}'
-#
+#data = '{"resultCount":5,"last":5,"success":true,"results":[{"trilat":51.22378540,"trilong":4.40892172,"ssid":"UAntwerpen","qos":7,"transid":"20120419-00187","firsttime":"2012-04-19T23:05:18.000Z","lasttime":"2015-02-22T11:26:18.000Z","lastupdt":"2015-02-22T09:28:02.000Z","netid":"00:0B:86:26:79:C5","name":null,"type":"infra","comment":null,"wep":"2","channel":6,"bcninterval":0,"freenet":"?","dhcp":"?","paynet":"?","userfound":false},{"trilat":51.22327042,"trilong":4.40893793,"ssid":"UAntwerpen","qos":4,"transid":"20120419-00187","firsttime":"2012-04-19T23:05:32.000Z","lasttime":"2014-03-09T07:15:11.000Z","lastupdt":"2014-03-09T05:15:16.000Z","netid":"00:0B:86:26:7A:75","name":null,"type":"infra","comment":null,"wep":"2","channel":6,"bcninterval":0,"freenet":"?","dhcp":"?","paynet":"?","userfound":false},{"trilat":51.22335815,"trilong":4.40887070,"ssid":"UAntwerpen","qos":7,"transid":"20120419-00187","firsttime":"2012-04-19T23:05:20.000Z","lasttime":"2016-04-24T13:59:46.000Z","lastupdt":"2016-04-24T14:00:33.000Z","netid":"00:0B:86:26:7E:75","name":null,"type":"infra","comment":null,"wep":"2","channel":11,"bcninterval":0,"freenet":"?","dhcp":"?","paynet":"?","userfound":false},{"trilat":51.22303772,"trilong":4.40889168,"ssid":"UAntwerpen","qos":0,"transid":"20140304-00114","firsttime":"2014-03-04T21:48:51.000Z","lasttime":"2014-03-05T19:18:58.000Z","lastupdt":"2014-03-05T17:19:23.000Z","netid":"00:0B:86:26:7E:D5","name":null,"type":"infra","comment":null,"wep":"2","channel":1,"bcninterval":0,"freenet":"?","dhcp":"?","paynet":"?","userfound":false},{"trilat":51.17709351,"trilong":4.41607475,"ssid":"UAntwerpen","qos":1,"transid":"20110605-00081","firsttime":"2011-05-31T17:11:13.000Z","lasttime":"2013-10-28T11:32:52.000Z","lastupdt":"2013-10-28T09:33:29.000Z","netid":"00:0b:86:26:86:16","name":null,"type":"infra","comment":null,"wep":"2","channel":6,"bcninterval":0,"freenet":"?","dhcp":"?","paynet":"?","userfound":false}],"first":1}'
 # latitude_index = data.find("trilat") + 8
 # latitude = data[latitude_index: (latitude_index+11)]
 # print("N"+latitude)
