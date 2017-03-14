4ed70ae53382317a165a6c548fc196e8443b6d73
thomasjanssenR8 <r8.thomas@hotmail.com>
2017-03-14 13:16:06 +0100

    Merging of files to  1 file 'main.py'

--- test.py
+++
@@ -1,5 +0,0 @@
-import haversine
-
-measurementCoordinate = (45.7597, 4.8422)
-meanCoordinate = (48.8567, 2.3508)
-print(haversine.haversine(measurementCoordinate, meanCoordinate))  # print difference in kilometers
