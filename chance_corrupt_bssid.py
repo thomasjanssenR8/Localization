dce9ef89b81cf4d6654891fdbff45393a10b708f
thomasjanssenR8 <r8.thomas@hotmail.com>
2017-05-08 16:30:52 +0200

    Calculate chance of combination above 500 meters for WiGLE and above 150 meters for LocationAPI

--- chance_corrupt_bssid.py
+++ chance_corrupt_bssid.py
@@ -15,34 +15,34 @@ def get_data():
     print('Amount of BSSIDs: ', amount_of_bssids)
 
     amount_of_bssid_combs = sheet.cell(row=end_row+3, column=4).value  # Get amount of possible combinations of 2 BSSIDs
-    print('Amount of combinations: ', amount_of_bssid_combs)  # @TODO change for LocationAPI
+    print('Amount of combinations: ', amount_of_bssid_combs)
 
-    begin_combs = end_row + 10  # @TODO change column for LocationAPI
+    begin_combs = end_row + 10
     end_combs = begin_combs + amount_of_bssid_combs - 1
     errors = []
 
     for i in range(begin_combs, end_combs+1):
-        if sheet.cell(row=i, column=7).value:  # @TODO change column for LocationAPI
-            errors.append(sheet.cell(row=i, column=7).value)       # Load distance errors (without None)# @TODO change column for LocationAPI
+        if sheet.cell(row=i, column=7).value:
+            errors.append(sheet.cell(row=i, column=7).value)       # Load distance errors (without None)
     read_data = [begin_combs, end_combs, errors]
     return read_data
 
 
-book_read = openpyxl.load_workbook(filename='data\\data_locationAPI_without_RSSI.xlsx')        # Load Excel sheet with all location errors # @TODO change for LocationAPI
+book_read = openpyxl.load_workbook(filename='data\\data_locationAPI_without_RSSI.xlsx')  # Load Excel sheets
 book_write = openpyxl.load_workbook(filename='data\\data_comparison.xlsx')
 sheet_write = book_write.get_sheet_by_name('Chance of corrupt BSSID')
 
-for location in range(1, 37):                                           # Load a template sheet for all 36 locations
+for location in range(1, 37):                                       # Load a template sheet for all 36 locations
     sheet = book_read.get_sheet_by_name('BAP' + str(location))
 
-    [begin_combs, end_combs, errors] = get_data()                       # read combination errors
+    [begin_combs, end_combs, errors] = get_data()                   # Read combination errors
     amount__of_corrupt_errors = 0
     for error in errors:
-        if error > 0.5:                                                 # threshold = 500 meters
+        if error > 0.150:                                           # Threshold = 500 m for WiGLE, 150 m for LocationAPI
             amount__of_corrupt_errors += 1
-    chance = amount__of_corrupt_errors / len(errors) * 100              # Calculate the chance of error above threshold
+    chance = amount__of_corrupt_errors / len(errors) * 100          # Calculate the chance of error above threshold
     print('The chance of a corrupt bssid is %.2f' % chance, '%')
-    sheet_write.cell(row=location+1, column=3).value = chance               # @TODO change column for LocationAPI
+    sheet_write.cell(row=location+1, column=3).value = chance
 
     book_write.save('data\\data_comparison.xlsx')
     print('BAP' + str(location) + ' saved.\n')
